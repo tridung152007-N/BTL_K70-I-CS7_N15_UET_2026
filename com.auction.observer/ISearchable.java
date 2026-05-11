f0bee3f69eadb7ae87eabe60d447a2fb153bc916
tridung152007-N <tridung152007@gmail.com>
2026-05-11 10:24:47 +0700

    Delete com.auction.observer directory

--- com.auction.observer/ISearchable.java
+++
@@ -1,8 +0,0 @@
-/**
- * Interface hỗ trợ tìm kiếm và lọc
- */
-package com.auction.observer;
-
-public interface ISearchable {
-    boolean match(String keyword); // Kiểm tra từ khóa khớp với tên/mô tả
-}
